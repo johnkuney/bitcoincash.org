512cf78781e6947d7ba0944bfd4ed4acf0a00b0f
John <jkuney17@gmail.com>
2020-08-31 11:46:10 -0500

    add link, clean up map

--- v2/src/components/tiles/tiles.js
+++ v2/src/components/tiles/tiles.js
@@ -1,25 +1,19 @@
 import React from "react"
 import { Container, Row, Col } from "react-bootstrap"
 import tilesStyles from "./tiles.module.css"
+import Link from "global/link.js"
 
 const TilesComponent = ({ tiles, md }) => {
   return (
     <Container>
       <Row>
-        {tiles.map(item => {
-          return (
-            <Col md={md} className={tilesStyles.tile}>
-              <a
-                href={item.link}
-                target="_blank"
-                rel="noreferrer"
-                alt={item.alt}
-              >
-                <img src={item.img} />
-              </a>
-            </Col>
-          )
-        })}
+        {tiles.map(item => (
+          <Col md={md} className={tilesStyles.tile}>
+            <Link to={item.link} target="_blank" rel="noreferrer">
+              <img src={item.img} alt={item.alt} />
+            </Link>
+          </Col>
+        ))}
       </Row>
     </Container>
   )
